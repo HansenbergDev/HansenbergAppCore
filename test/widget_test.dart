9cc918e8eae966d5b143c07f291a822b43214cfa
Ane Jørgensen <anesoejoe@gmail.com>
2022-10-04 16:03:35 +0200

    Maybe this works?

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hansenberg_app_core/something.dart';
+import '../lib/something.dart';
 
 void main() {
   myTest(3);
