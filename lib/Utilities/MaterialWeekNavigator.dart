1aa5e390855fe507850616b9054435f46e959efa
Ane Jørgensen <anesoejoe@gmail.com>
2023-01-31 19:38:08 +0100

    Moved WeekNavigator

---
+++ lib/Utilities/MaterialWeekNavigator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WeekNavigator {
+  const WeekNavigator({required this.context, required this.week});
+
+  final BuildContext context;
+  final int week;
+
+  void toNextWeek() {
+    _navigateToWeek(week + 1);
+  }
+
+  void toPreviousWeek() {
+    _navigateToWeek(week - 1);
+  }
+
+  void _navigateToWeek(int week) {
+    Navigator.of(context).pushReplacementNamed('$week');
+  }
+}
