5b37005187e36fe673e462ffdc44364fe8390954
Ane Jørgensen <anesoejoe@gmail.com>
2023-01-31 18:16:53 +0100

    Removed prints and unused code, Uri.http now works (thanks to CORS)

--- lib/Utilities/Clients/HttpClient.dart
+++ lib/Utilities/Clients/HttpClient.dart
@@ -12,56 +12,31 @@ class HttpClient {
   static const Duration defaultTimeoutDuration = Duration(seconds: 1);
 
   Future<http.Response> get({required String endpoint, Map<String, String> params = const {}, Map<String, String> headers = const {}}) {
-    // String formattedParams = "";
-    //
-    // if (params.isNotEmpty) {
-    //   formattedParams = "?";
-    //
-    //   for (var key in params.keys) {
-    //     formattedParams = "$formattedParams$key=${params[key]}&";
-    //   }
-    //
-    //   formattedParams = formattedParams.substring(0, formattedParams.length - 1);
-    // }
-
-    // final path = '$base$endpoint$formattedParams';
-    final uri = Uri.http(urlBase, '$endpointBase$endpoint', params);
-    print(uri);
-
     return http.get(
-        uri,
+        Uri.http(urlBase, '$endpointBase$endpoint', params),
         headers: headers
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
   }
 
   Future<http.Response> post({required String endpoint, required Map<String, String> headers, required Map<String, dynamic> body}) {
-    final uri = Uri.http(urlBase, '$endpointBase$endpoint');
-    print(uri);
     return http.post(
-      // Uri.parse("$base$endpoint"),
-      uri,
+      Uri.http(urlBase, '$endpointBase$endpoint'),
       headers: headers,
       body: jsonEncode(body),
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
   }
 
   Future<http.Response> patch({required String endpoint, required Map<String, String> headers, required Map<String, dynamic> body}) {
-    final uri = Uri.http(urlBase, '$endpointBase$endpoint');
-    print(uri);
     return http.patch(
-        // Uri.parse("$base$endpoint"),
-        uri,
+        Uri.http(urlBase, '$endpointBase$endpoint'),
         headers: headers,
         body: jsonEncode(body)
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
   }
 
   Future<http.Response> delete({required String endpoint, required Map<String, String> headers, Map<String, dynamic> body = const {}}) {
-    final uri = Uri.http(urlBase, '$endpointBase$endpoint');
-    print(uri);
     return http.delete(
-        // Uri.parse("$base$endpoint"),
-        uri,
+        Uri.http(urlBase, '$endpointBase$endpoint'),
         headers: headers,
         body: jsonEncode(body)
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
