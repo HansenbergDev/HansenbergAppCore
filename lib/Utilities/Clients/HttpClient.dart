7fd8416253334d4399e38e56f75365aae1f78ede
Ane Jørgensen <anesoejoe@gmail.com>
2023-02-01 18:43:25 +0100

    Trying a switch to HTTPS

--- lib/Utilities/Clients/HttpClient.dart
+++ lib/Utilities/Clients/HttpClient.dart
@@ -13,14 +13,14 @@ class HttpClient {
 
   Future<http.Response> get({required String endpoint, Map<String, String> params = const {}, Map<String, String> headers = const {}}) {
     return http.get(
-        Uri.http(urlBase, '$endpointBase$endpoint', params),
+        Uri.https(urlBase, '$endpointBase$endpoint', params),
         headers: headers
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
   }
 
   Future<http.Response> post({required String endpoint, required Map<String, String> headers, required Map<String, dynamic> body}) {
     return http.post(
-      Uri.http(urlBase, '$endpointBase$endpoint'),
+      Uri.https(urlBase, '$endpointBase$endpoint'),
       headers: headers,
       body: jsonEncode(body),
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
@@ -28,7 +28,7 @@ class HttpClient {
 
   Future<http.Response> patch({required String endpoint, required Map<String, String> headers, required Map<String, dynamic> body}) {
     return http.patch(
-        Uri.http(urlBase, '$endpointBase$endpoint'),
+        Uri.https(urlBase, '$endpointBase$endpoint'),
         headers: headers,
         body: jsonEncode(body)
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
@@ -36,7 +36,7 @@ class HttpClient {
 
   Future<http.Response> delete({required String endpoint, required Map<String, String> headers, Map<String, dynamic> body = const {}}) {
     return http.delete(
-        Uri.http(urlBase, '$endpointBase$endpoint'),
+        Uri.https(urlBase, '$endpointBase$endpoint'),
         headers: headers,
         body: jsonEncode(body)
     ).timeout(defaultTimeoutDuration, onTimeout: () { return http.Response('Error', 408); });
